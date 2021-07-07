005234f25007a34b8dbe71f5358bc1a589e00a4a
ckk9296 <ckk9296@gmail.com>
2021-07-07 08:50:48 -0400

    added react code

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+
+  hiClicked(){
+    alert('helo kishore')
+  }
+
+  hiClickedWithParams(data){
+    alert('hi'+data)
+  }
+addNum(num1){
+  let num2=2;
+  let num3=num1*num2;
+  alert (num3);
+}
+
+  render(){
+
+    return(
+      <div>
+        <h1 onClick={this.hiClicked}>Hello kishore</h1>
+      <h1 onClick={()=>this.hiClickedWithParams('kishore how are you')}>hi </h1>
+
+      <h1 onClick={()=>this.addNum(2)}>add number s</h1>
+
+      </div>
+    )
+  }
+
+}
+export default App;
